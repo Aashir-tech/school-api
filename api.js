28fbf38df2e1fddb4cd7d69ae49961962d8dd385
Aashir-tech <aashirharis6@gmail.com>
2025-04-23 21:30:55 +0530

    Fix

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const app = require('./index');
-const serverless = require('serverless-http');
+const app = require("./api/index");
+const serverless = require("serverless-http");
 
 export const handler = serverless(app);
---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+const schoolRoutes = require("../routes/school");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to School API , I am Aashir Haris",
+  });
+});
+
+app.use("/api", schoolRoutes);
+
+// app.listen(PORT , () => {
+//     console.log("Server is running on port" , PORT )
+// })
+
+export default app;
